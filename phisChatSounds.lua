ff81b06db417d30f210035c0cde7e1ceb8e36ce1
Moritz <phis@mogobb.de>
2021-04-08 09:58:38 +0200

    Sounds changed; two simultaneously to hear them better

--- phisChatSounds.lua
+++ phisChatSounds.lua
@@ -5,8 +5,9 @@ local phisFrame = CreateFrame('Frame', addonName..'CheckFrame', UIParent)
 phisFrame:RegisterEvent("CHAT_MSG_GUILD")
 phisFrame:RegisterEvent("CHAT_MSG_OFFICER")
 
-phisFrame:SetScript("OnEvent", function(self, event, ...) 
-	-- PlaySound(111366, "master") -- UI_VoiceChat_ChatMessageIncoming
-	-- PlaySound(111365, "master") -- UI_VoiceChat_ChannelInitiated
-	PlaySound(3081, "master") -- TellMessage
+phisFrame:SetScript("OnEvent", function(self, event, ...)
+	-- play both simultaneously for louder volume
+	PlaySound(111366, "master") -- UI_VoiceChat_ChatMessageIncoming
+	PlaySound(111365, "master") -- UI_VoiceChat_ChannelInitiated
+	-- PlaySound(3081, "master") -- TellMessage
 end)
